a87261235900cce2756e1749dd2e198a3b1b43d6
km16f <km16f@my.fsu.edu>
2019-12-04 10:41:35 -0500

    more

--- src/bin/rankings.py
+++ src/bin/rankings.py
@@ -1,6 +1,6 @@
 import sys
 import src.bin.util
-from PyQt5.QtWidgets import QTableWidget, QWidget, QApplication, QVBoxLayout, QHeaderView, QTableWidgetItem
+from PyQt5.QtWidgets import QTableWidget, QWidget, QApplication, QVBoxLayout, QHeaderView, QTableWidgetItem, QLineEdit, QPushButton, QHBoxLayout, QLabel
 import PyQt5.QtGui
 
 
@@ -48,7 +48,60 @@ class AddTeam(QWidget):
         self.setWindowTitle("Add Team")
         self.setGeometry(600, 200, 200, 200)
 
+        self.submit = QPushButton("Submit")
+        self.label1 = QLabel("Enter Team Location: ")
+        self.label2 = QLabel("Enter Team Mascot: ")
+        self.label3 = QLabel("Enter Team Wins: ")
+        self.label4 = QLabel("Enter Team Losses: ")
+        self.label5 = QLabel("Enter Team Rank: ")
+        self.label6 = QLabel("Enter Division: ")
+
+        self.h1 = QHBoxLayout()
+        self.h2 = QHBoxLayout()
+        self.h3 = QHBoxLayout()
+        self.h4 = QHBoxLayout()
+        self.h5 = QHBoxLayout()
+        self.h6 = QHBoxLayout()
+
         self.main_layout1 = QVBoxLayout()
+        self.line1 = QLineEdit()
+        self.line2 = QLineEdit()
+        self.line3 = QLineEdit()
+        self.line4 = QLineEdit()
+        self.line5 = QLineEdit()
+        self.line6 = QLineEdit()
+
+        self.h1.addWidget(self.label1)
+        self.h1.addWidget(self.line1)
+
+        self.h2.addWidget(self.label2)
+        self.h2.addWidget(self.line2)
+
+        self.h3.addWidget(self.label3)
+        self.h3.addWidget(self.line3)
+
+        self.h4.addWidget(self.label4)
+        self.h4.addWidget(self.line4)
+
+        self.h5.addWidget(self.label5)
+        self.h5.addWidget(self.line5)
+
+        self.h6.addWidget(self.label6)
+        self.h6.addWidget(self.line6)
+
+        self.main_layout1.addLayout(self.h1)
+        self.main_layout1.addLayout(self.h2)
+        self.main_layout1.addLayout(self.h3)
+        self.main_layout1.addLayout(self.h4)
+        self.main_layout1.addLayout(self.h5)
+        self.main_layout1.addLayout(self.h6)
+
+        self.submit.clicked.connect(self.submit)
+
+    def submit(self):
+        src.bin.util.insert_rankings(self.label1.text(), self.label2.text(), self.label3.text(), self.label4.text(), self.label5.text())
+        src.bin.util.insert_team(self.label1.text(), self.label2.text(), self.label6.text())
+
 
 
 class BadTeams(QWidget):
